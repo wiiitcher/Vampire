51001b3e28409b41c550d8713de11acc95df3315
geralt of rivia <112061724+wiiitcher@users.noreply.github.com>
2023-07-31 01:32:45 +0330

    Update index.js
    Update contract address

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xab008d2d9a869ccc8af6c6c4bae0f1bea4291598";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
